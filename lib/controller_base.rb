aac51d580bd9cb019e79a6924f6c188f2c235ed8
Michael Cittar <mcittar@Michaels-MacBook-Air.local>
2017-02-21 17:16:58 -0800

    Initial commit

---
+++ lib/controller_base.rb
@@ -0,0 +1,68 @@
+require 'active_support'
+require 'active_support/core_ext'
+require 'active_support/inflector'
+require 'erb'
+require_relative './session'
+require 'byebug'
+
+class ControllerBase
+  attr_reader :req, :res, :params
+
+  # Setup the controller
+  def initialize(req, res)
+    @req = req
+    @res = res
+  end
+
+  # Helper method to alias @already_built_response
+  def already_built_response?
+    @already_built_response
+  end
+
+  # Set the response status code and header
+  def redirect_to(url)
+    raise "Double render error" if already_built_response?
+
+    @res.status = 302
+    @res.set_header('Location', url)
+    @session.store_session(@res)
+
+    @already_built_response = true
+  end
+
+  # Populate the response with content.
+  # Set the response's content type to the given type.
+  # Raise an error if the developer tries to double render.
+  def render_content(content, content_type)
+    raise "Double render error" if already_built_response?
+
+    @res.write(content)
+    @res['Content-Type'] = content_type
+    @session.store_session(@res)
+    @already_built_response = true
+  end
+
+  # use ERB and binding to evaluate templates
+  # pass the rendered html to render_content
+  def render(template_name)
+    raise "Double render error" if already_built_response?
+
+    controller_name = ActiveSupport::Inflector.underscore("#{self.class}")
+    file = File.read("views/#{controller_name}/#{template_name}.html.erb")
+
+    render_content(ERB.new(file).result(binding), 'text/html')
+
+    @already_built_response = true
+  end
+
+  # method exposing a `Session` object
+  def session
+    @session ||= Session.new(@req)
+  end
+
+  # use this with the router to call action_name (:index, :show, :create...)
+  def invoke_action(name)
+    send(name)
+    render(name) unless already_built_response?
+  end
+end
